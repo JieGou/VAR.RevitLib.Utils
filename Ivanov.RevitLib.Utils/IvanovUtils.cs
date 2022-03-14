7a6ca8a6698fd25414539656975fa5009def5f36
Leonardo2526 <leonardo147wot@gmail.com>
2022-03-14 19:39:52 +0300

    IvanovUtils renaming

--- Ivanov.RevitLib.Utils/IvanovUtils.cs
+++ Ivanov.RevitLib.Utils/IvanovUtils.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace DS.RevitLib.Utils.External
+namespace Ivanov.RevitLib.Utils
 {
     public static class IvanovUtils
     {
